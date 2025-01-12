6fa7fe9af2e86feeb23895c8c95c912d7dff04d5
dobeyc3 <christopher.dobey3@mail.dcu.ie>
2025-01-12 23:00:17 +0000

    Updated degree invokation and validation scripts to work woith new orgs

--- src/ledger/fabric-samples/test-network/scripts/invokeDegree.sh
+++ src/ledger/fabric-samples/test-network/scripts/invokeDegree.sh
@@ -3,12 +3,12 @@
 # Load environment variables
 source .env
 
-# Set the peer environment variables for Org1
-export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID=$ORG1_MSPID
-export CORE_PEER_MSPCONFIGPATH=$ORG1_MSP_PATH
-export CORE_PEER_TLS_ROOTCERT_FILE=$ORG1_TLS_CERT
-export CORE_PEER_ADDRESS=$ORG1_ADDRESS
+# Set the peer environment variables for OrgUniversity
+export CORE_PEER_TLS_ENABLED=$ORG_UNIVERSITY_TLS_ENABLED
+export CORE_PEER_LOCALMSPID=$ORG_UNIVERSITY_MSPID
+export CORE_PEER_MSPCONFIGPATH=$ORG_UNIVERSITY_MSP_PATH
+export CORE_PEER_TLS_ROOTCERT_FILE=$ORG_UNIVERSITY_TLS_CERT
+export CORE_PEER_ADDRESS=$ORG_UNIVERSITY_ADDRESS
 
 peer chaincode invoke -o localhost:7050 --tls --cafile $ORDERER_CA \
 -C mychannel -n degreeCC $PEER_ADDRESSES \
--- src/ledger/fabric-samples/test-network/scripts/queryDegree.sh
+++ src/ledger/fabric-samples/test-network/scripts/queryDegree.sh
@@ -3,16 +3,11 @@
 # Load environment variables
 source .env
 
-# Set the peer environment variables for Org1
-export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID=$ORG1_MSPID
-export CORE_PEER_MSPCONFIGPATH=$ORG1_MSP_PATH
-export CORE_PEER_TLS_ROOTCERT_FILE=$ORG1_TLS_CERT
-export CORE_PEER_ADDRESS=$ORG1_ADDRESS
+# Set the peer environment variables for OrgUniversity
+export CORE_PEER_TLS_ENABLED=$ORG_UNIVERSITY_TLS_ENABLED
+export CORE_PEER_LOCALMSPID=$ORG_UNIVERSITY_MSPID
+export CORE_PEER_MSPCONFIGPATH=$ORG_UNIVERSITY_MSP_PATH
+export CORE_PEER_TLS_ROOTCERT_FILE=$ORG_UNIVERSITY_TLS_CERT
+export CORE_PEER_ADDRESS=$ORG_UNIVERSITY_ADDRESS
 
-peer chaincode query -C mychannel -n degreeCC -c '{"Args":["ReadDegree","test-12345"]}'
-if [ $? -eq 0 ]; then
-    echo "QUERY SUCCESSFUL"
-else
-    echo "QUERY FAILED"
-fi
+peer chaincode query -C mychannel -n degreeCC -c '{"Args":["ReadDegree","1"]}'
