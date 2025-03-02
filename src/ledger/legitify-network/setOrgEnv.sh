9f954f32b294806d755f57083120421acb207b23
dobeyc3 <christopher.dobey3@mail.dcu.ie>
2025-03-02 15:45:27 +0000

    Revert "updated network setup scripts to use ip instead opf localhost ports"
    This reverts commit 9306cc176d4b817dbabe55d819d7c4de74c10a66.

--- src/ledger/legitify-network/setOrgEnv.sh
+++ src/ledger/legitify-network/setOrgEnv.sh
@@ -91,21 +91,21 @@ if [[ ${ORG,,} == "orguniversity" ]]; then
 
    CORE_PEER_LOCALMSPID=OrgUniversityMSP
    CORE_PEER_MSPCONFIGPATH=${DIR}/legitify-network/organizations/peerOrganizations/orguniversity.com/users/Admin@orguniversity.com/msp
-   CORE_PEER_ADDRESS=176.34.66.195:7051
+   CORE_PEER_ADDRESS=localhost:7051
    CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/legitify-network/organizations/peerOrganizations/orguniversity.com/tlsca/tlsca.orguniversity.com-cert.pem
 
 elif [[ ${ORG,,} == "orgemployer" ]]; then
 
    CORE_PEER_LOCALMSPID=OrgEmployerMSP
    CORE_PEER_MSPCONFIGPATH=${DIR}/legitify-network/organizations/peerOrganizations/orgemployer.com/users/Admin@orgemployer.com/msp
-   CORE_PEER_ADDRESS=176.34.66.195:8051
+   CORE_PEER_ADDRESS=localhost:8051
    CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/legitify-network/organizations/peerOrganizations/orgemployer.com/tlsca/tlsca.orgemployer.com-cert.pem
 
 elif [[ ${ORG,,} == "orgindividual" ]]; then
 
    CORE_PEER_LOCALMSPID=OrgIndividualMSP
    CORE_PEER_MSPCONFIGPATH=${DIR}/legitify-network/organizations/peerOrganizations/orgindividual.com/users/Admin@orgindividual.com/msp
-   CORE_PEER_ADDRESS=176.34.66.195:9051
+   CORE_PEER_ADDRESS=localhost:9051
    CORE_PEER_TLS_ROOTCERT_FILE=${DIR}/legitify-network/organizations/peerOrganizations/orgindividual.com/tlsca/tlsca.orgindividual.com-cert.pem
 
 else
